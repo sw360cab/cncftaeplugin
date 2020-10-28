5648d16b62276762b35fb5e4f6fe90b5c1924bac
sw360cab <sw360cab@gmail.com>
2020-10-28 15:39:27 +0100

    init commit

---
+++ demo.go
@@ -0,0 +1,57 @@
+package uiddemo
+
+import (
+  "fmt"
+  "context"
+  "net/http"
+  "gitlab.com/rwxrob/uniq"
+)
+
+const defaultHeader = "X-Traefik-Uuid"
+
+// Config holds configuration to be passed to the plugin
+type Config struct {
+  HeaderName string
+}
+
+// CreateConfig populates the Config data object
+func CreateConfig() *Config {
+  return &Config{
+    HeaderName: defaultHeader,
+  }
+}
+
+// UIDDemo holds the necessary components of a Traefik plugin
+type UIDDemo struct {
+  headerName string
+  next http.Handler
+  name string
+}
+
+// New instantiates and returns the required components used to handle a HTTP request
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+  if len(config.HeaderName) == 0 {
+    return nil, fmt.Errorf("HeaderName cannot be empty")
+  }
+
+  return &UIDDemo{
+    headerName: config.HeaderName,
+    next: next,
+    name: name,
+  }, nil
+}
+
+func (u *UIDDemo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+  uid := uniq.UUID()
+  cookieVal := "Traefik-Cookie=" + uid + ";Max-Age=86400"
+
+  // header injection to backend service
+  req.Header.Set(u.headerName, uid)
+  // header injection to client response
+  rw.Header().Add(u.headerName, uid)
+  rw.Header().Add("Set-Cookie", cookieVal)
+
+  fmt.Println("Set cookie:", cookieVal) 
+
+  u.next.ServeHTTP(rw, req)
+}
